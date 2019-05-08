fe4272954646e6e6a0bd7df6bd2d2a517ff96ab5
danilocbueno <danilocbueno@gmail.com>
2019-05-08 17:08:37 -0300

    exemplos e tutorial

--- controlador/paginasControlador.php
+++ controlador/paginasControlador.php
@@ -3,6 +3,6 @@
 /* CONTROLADOR
  * funçao: controlar as páginas estáticas (páginas sem acesso ao modelo)  */
 
-function index() {
-    exibir("paginas/inicial");
+function sobre() {
+    exibir("paginas/sobre");
 }
